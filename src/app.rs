eb499de7292e48b7917ec855e0d5bb1f2397d79b
justinac0 <justinac.1999@gmail.com>
2025-05-12 08:24:46 +1000

    Adding basic ui 	modified:   Cargo.lock 	modified:   Cargo.toml 	modified:   src/app.rs 	modified:   src/lib.rs 	modified:   src/main.rs

--- src/app.rs
+++ src/app.rs
@@ -1,109 +1,102 @@
-/// We derive Deserialize/Serialize so we can persist app state on shutdown.
-#[derive(serde::Deserialize, serde::Serialize)]
-#[serde(default)] // if we add new fields, give them default values when deserializing old state
-pub struct TemplateApp {
-    // Example stuff:
-    label: String,
+use std::time::Duration;
 
-    #[serde(skip)] // This how you opt-out of serialization of a field
-    value: f32,
+#[derive(serde::Deserialize, serde::Serialize)]
+#[serde(default)]
+pub struct SerialEmbedApp {
+    #[serde(skip)]
+    current_port: Option<Box<dyn serialport::SerialPort + 'static>>,
+    ports: Vec<String>,
+    console: Vec<String>,
+    message: String,
 }
 
-impl Default for TemplateApp {
+impl Default for SerialEmbedApp {
     fn default() -> Self {
         Self {
-            // Example stuff:
-            label: "Hello World!".to_owned(),
-            value: 2.7,
+            current_port: None,
+            ports: Vec::new(),
+            console: Vec::new(),
+            message: String::new(),
         }
     }
 }
 
-impl TemplateApp {
-    /// Called once before the first frame.
-    pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
-        // This is also where you can customize the look and feel of egui using
-        // `cc.egui_ctx.set_visuals` and `cc.egui_ctx.set_fonts`.
-
-        // Load previous app state (if any).
-        // Note that you must enable the `persistence` feature for this to work.
-        if let Some(storage) = cc.storage {
-            return eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default();
-        }
-
+impl SerialEmbedApp {
+    pub fn new(_: &eframe::CreationContext<'_>) -> Self {
         Default::default()
     }
 }
 
-impl eframe::App for TemplateApp {
-    /// Called by the frame work to save state before shutdown.
-    fn save(&mut self, storage: &mut dyn eframe::Storage) {
-        eframe::set_value(storage, eframe::APP_KEY, self);
-    }
-
-    /// Called each time the UI needs repainting, which may be many times per second.
+impl eframe::App for SerialEmbedApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        // Put your widgets into a `SidePanel`, `TopBottomPanel`, `CentralPanel`, `Window` or `Area`.
-        // For inspiration and more examples, go to https://emilk.github.io/egui
-
-        egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
-            // The top panel is often a good place for a menu bar:
-
-            egui::menu::bar(ui, |ui| {
-                // NOTE: no File->Quit on web pages!
-                let is_web = cfg!(target_arch = "wasm32");
-                if !is_web {
-                    ui.menu_button("File", |ui| {
-                        if ui.button("Quit").clicked() {
-                            ctx.send_viewport_cmd(egui::ViewportCommand::Close);
-                        }
-                    });
-                    ui.add_space(16.0);
-                }
-
-                egui::widgets::global_theme_preference_buttons(ui);
-            });
-        });
 
         egui::CentralPanel::default().show(ctx, |ui| {
-            // The central panel the region left after adding TopPanel's and SidePanel's
-            ui.heading("eframe template");
+            let available_width = ui.available_width();
+            let left_width = available_width / 3.0;
+            let right_width = available_width - left_width;
+
+            ui.set_width(available_width);
+            ui.heading("SerialEmbed");
 
             ui.horizontal(|ui| {
-                ui.label("Write something: ");
-                ui.text_edit_singleline(&mut self.label);
+                ui.label("Send Message: ");
+                ui.text_edit_singleline(&mut self.message);
+                if ui.button("Send").clicked() {
+                    if self.current_port.is_some() {
+                        let msg = self.message.as_bytes(); 
+                        let port = self.current_port.as_mut().unwrap();
+                        let result = port.write(msg);
+                        if result.is_ok() {
+                            println!("{:?}", result);
+                        }
+                    }
+                }
             });
-
-            ui.add(egui::Slider::new(&mut self.value, 0.0..=10.0).text("value"));
-            if ui.button("Increment").clicked() {
-                self.value += 1.0;
-            }
 
             ui.separator();
 
-            ui.add(egui::github_link_file!(
-                "https://github.com/emilk/eframe_template/blob/main/",
-                "Source code."
-            ));
+            ui.horizontal(|ui| {
+                ui.set_width(available_width);
+                // config panel
+                if self.current_port.is_none() {
+                    ui.vertical(|ui| {
+                        if ui.button("Scan COM Ports").clicked() {
+                            ui.set_width(left_width);
+                            
+                            let ports = serialport::available_ports().unwrap();
+                            self.ports.clear();
+                            for p in ports {
+                                self.ports.push(p.port_name);
+                            }
+                        }
+                        
+                    for p in &self.ports {
+                        let name = p.as_str();
 
-            ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
-                powered_by_egui_and_eframe(ui);
-                egui::warn_if_debug_build(ui);
+                        ui.horizontal(|ui| {
+                        ui.label(name);
+                            if ui.button("Open").clicked() {
+                                self.current_port = Some(serialport::new(name, 115_200)
+                                    .timeout(Duration::from_millis(100))
+                                    .open()
+                                    .expect("Failed to open port"));
+                            }
+                        });
+                    }
+                });
+            }
+
+                // content panel
+                ui.vertical(|ui| {
+                    ui.set_width(right_width);
+                    ui.label("Right panel (2/3)");
+                });
+            });
+
+            // footer
+            ui.with_layout(egui::Layout::bottom_up(egui::Align::RIGHT), |ui| {
+                ui.hyperlink("https://justinchappell.com");
             });
         });
     }
-}
-
-fn powered_by_egui_and_eframe(ui: &mut egui::Ui) {
-    ui.horizontal(|ui| {
-        ui.spacing_mut().item_spacing.x = 0.0;
-        ui.label("Powered by ");
-        ui.hyperlink_to("egui", "https://github.com/emilk/egui");
-        ui.label(" and ");
-        ui.hyperlink_to(
-            "eframe",
-            "https://github.com/emilk/egui/tree/master/crates/eframe",
-        );
-        ui.label(".");
-    });
 }
